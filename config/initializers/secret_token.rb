23f37c9cb9bc8d8621f330fd23ea4418db674c36
sven199109 <sven@sven-VirtualBox.(none)>
2014-01-19 14:35:14 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microposts::Application.config.secret_token = '245624c5edd729886464b4fa6a3a75aa38d8efe15bd0702df546cda2e8fb21888a54b8ea85f878fb76ae164f567276289b33ccfd7cb1b0c45476fdc4f81f7220'
