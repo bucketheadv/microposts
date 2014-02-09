f13d9a1be24a2a55fb22b182b9556bfc8e52488c
sven199109 <sven@sven-VirtualBox.(none)>
2014-02-09 18:46:47 +0800

    记住我

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,13 @@ class SessionsController < ApplicationController
 	def create 
 		user = User.find_by_email(params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
-			sign_in user
+			if params[:session][:remember_token].to_i == 1
+				#flash.now.alert = "永久登录"
+				sign_in_permanent user
+			else
+				#flash.now.alert = "临时登录"
+				sign_in user
+			end
 			redirect_back_or user
 		else
 			flash.now[:error] = "用户名或密码不正确"
